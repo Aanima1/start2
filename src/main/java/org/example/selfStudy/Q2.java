f665bb2a1df53f53125c588711f0e3433537dc96
kimjuil <wndlfwndlf10@naver.com>
2024-09-25 23:45:35 +0900

    start1
    first java class

---
+++ src/main/java/org/example/selfStudy/Q2.java
@@ -0,0 +1,23 @@
+package org.example.selfStudy;
+
+public class Q2 {
+    public static void main(String[] args) {
+
+        int age = 30;
+        double height = 175.5;
+        double weight = 70.2;
+        String add = "123 Main St, Anytown, USA";
+        String phNbr = "010-1234-5678";
+        final char SEX = 'F';
+        int math = 100;
+
+
+        System.out.println("나이 : "+age+"세");
+        System.out.println("키 : "+height+"cm");
+        System.out.println("몸무게 : "+weight+"kg");
+        System.out.println("주소 : "+add);
+        System.out.println("전화번호 : "+phNbr);
+        System.out.println("성별 : "+SEX);
+        System.out.println("수학점수 : "+math+"점");
+    }
+}
